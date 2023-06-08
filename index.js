9a21db6abf50e190449a3a16091c07690a9c8e73
Syed-Anwarul-Haque-Piash <syed88783@gmail.com>
2023-06-08 15:08:03 +0600

    put api

---
+++ index.js
@@ -0,0 +1,67 @@
+const express = require('express')
+const cors=require('cors');
+const app = express();
+require('dotenv').config();
+const port = process.env.PORT || 5000;
+//app.use(cors());
+app.use(express.json());
+const corsOptions = {
+  origin: '*',
+  credentials: true,
+  optionSuccessStatus: 200,
+}
+app.use(cors(corsOptions))
+app.use(express.json())
+//Amping
+//CBOWMU7KdvC5GwaG
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.4doag.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    const usersCollection=client.db('Amping').collection('users');
+
+    app.put('/users/:email',async(req,res)=>{
+        const email=req.params.email;
+        const user=req.body;
+        const query={email:email}
+        const options={upsert:true}
+        const updateDoc={
+            $set:user
+        }
+        const result=await usersCollection.updateOne(query,updateDoc, options);
+        res.send(result)
+    })
+    app.get('/users',async(req,res)=>{
+      const result=await usersCollection.find({}).toArray();
+      res.send(result);
+    })
+   
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    //await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World!I am comming')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
